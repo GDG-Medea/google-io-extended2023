5e5c0269544eec19757b308844ef7547e02e766c
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-11 20:49:48 +0100

    change color to tailwind instead of shortcut

--- src/components/Carousel/items.tsx
+++ src/components/Carousel/items.tsx
@@ -4,20 +4,20 @@ const items = [
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         img: "/speaker1",
-        color: "ggreen"
+        color: "bg-gdgyellow-500"
 
     },
     {
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         img: "/speaker2",
-        color: "gyellow"
+        color: "bg-gdgred-500"
     },
     {
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         img: "/speaker3",
-        color: "gblue"
+        color: "bg-gdgblue-500"
     }
 ]
 export default items
