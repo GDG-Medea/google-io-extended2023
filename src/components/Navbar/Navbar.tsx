955fbdec9548e1ccdf9023405d171bca86ac2edc
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-11 20:50:20 +0100

    add hide on scroll down to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const isVisible = prevScrollPos > currentScrollPos;
+
+        setPrevScrollPos(currentScrollPos);
+        setVisible(isVisible);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollPos]);
     return (
-        <nav className="p-3 lg:p-6 w-full shadow-md sticky top-0 z-50 bg-bglight dark:bg-bgdark">
+        <nav className={`p-3 lg:p-6 w-full shadow-md sticky  z-50 bg-bglight dark:bg-bgdark ${visible ? 'top-0' : '-top-20'} transition duration-300 ease-in-out`}>
             <div className="flex justify-between">
                 <div className="flex  items-center gap-2">
-                    <img src="/gdglogo.jpg" className="h-10 px-2 dark:invert bg-blend-overlay" />
+                    <img src="/gdglogo.jpg" className="h-10 px-2 dark:invert bg-blend-overlay rounded-xl" />
                     <div className="">Google I/O Extended</div>
                 </div>
                 <div>
-                    <button className="px-4 p-2 rounded-xl hover:border-white border-4 border-transparent  hover:bg-ggreen-600 dark:hover:bg-ggreen-500 dark:text-white dark:bg-ggreen-500 bg-ggreen-500">
+                    <button className="px-4 p-2 rounded-xl hover:border-white border-4 border-transparent  hover:bg-gdggreen-600 dark:hover:bg-gdggreen-500 dark:text-white dark:bg-gdggreen-500 bg-gdggreen-500">
                         <a href="https://linktoform.com">Join Us here!</a>
                     </button>
                 </div>
