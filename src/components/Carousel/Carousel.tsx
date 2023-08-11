bf61db4369efe5e4b72c4f816edb15c64c8aae0a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-11 20:49:32 +0100

    change card color to include full tailwind class

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,24 +4,28 @@ import items from "./items"
 
 const OurCarousel = () => {
     return (
-        <Carousel loop={true}>
-            {items.map((item, index) => {
-                return (
-                    < div className={`bg-${item.color}-500 rounded-3xl text-white  h-full w-full flex flex-col items-center justify-center gap-5 p-12`
-                    } key={index} >
-                        <img
-                            // src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                            src={`/speakers/speaker${index + 1}.png`}
-                            alt={`"image ${index + 1}"`}
-                            className=" object-cover  w-32 h-32  rounded-full border-4"
-                        />
-                        <div className="text-3xl font-bold">{item.name}</div>
-                        <div>{item.bio}</div>
-                    </div>
-                )
-            })}
+        <div id="content" className='mx-2 w-11/12 p-2 flex flex-col items-center justify-center'>
+            <div className='w-full lg:w-2/5 flex flex-col items-center gap-4'>
+                <div className='text-4xl font-bold'>Meet Our speakers</div>
+                <Carousel loop={true}>
+                    {items.map((item, index) => {
+                        return (
+                            < div className={`${item.color}  bg-gdgyellow-500 rounded-3xl text-white  h-full w-full flex flex-col items-center justify-center gap-5 p-12`
+                            } key={index}   >
+                                <img
+                                    src={`/speakers/speaker${index + 1}.png`}
+                                    alt={`"image ${index + 1}"`}
+                                    className=" object-cover  w-32 h-32  rounded-full border-4"
+                                />
+                                <div className="text-3xl font-bold">{item.name}</div>
+                                <div>{item.bio}</div>
+                            </div>
+                        )
+                    })}
 
-        </Carousel >
+                </Carousel >
+            </div >
+        </div >
     );
 }
 export default OurCarousel
