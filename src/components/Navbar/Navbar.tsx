104911a0fdb4ec283e24458be401fcb6a088f877
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-11 18:00:48 +0100

    add elements to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,17 @@
 const Navbar = () => {
     return (
-        <nav>
-            GDG Logo
+        <nav className="p-3 lg:p-6 w-full shadow-md sticky top-0 z-50 bg-bglight dark:bg-bgdark">
+            <div className="flex justify-between">
+                <div className="flex  items-center gap-2">
+                    <img src="/gdglogo.jpg" className="h-10 px-2 dark:invert bg-blend-overlay" />
+                    <div className="">Google I/O Extended</div>
+                </div>
+                <div>
+                    <button className="px-4 p-2 rounded-xl hover:border-white border-4 border-transparent  hover:bg-ggreen-600 dark:hover:bg-ggreen-500 text-white dark:bg-ggreen-400 bg-ggreen-500">
+                        <a href="https://linktoform.com">Join Us here!</a>
+                    </button>
+                </div>
+            </div>
         </nav>
     )
 }
