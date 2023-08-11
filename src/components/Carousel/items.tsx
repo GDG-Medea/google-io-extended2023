0d368a591e003dcb6b417919806859fa7137e6c3
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-11 18:00:34 +0100

    ceate item list of speakers

---
+++ src/components/Carousel/items.tsx
@@ -0,0 +1,23 @@
+
+const items = [
+    {
+        name: "Lorem ipsum",
+        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "/speaker1",
+        color: "ggreen"
+
+    },
+    {
+        name: "Lorem ipsum",
+        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "/speaker2",
+        color: "gyellow"
+    },
+    {
+        name: "Lorem ipsum",
+        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "/speaker3",
+        color: "gblue"
+    }
+]
+export default items
