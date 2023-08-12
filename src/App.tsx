cd9f941b5851a6256d5f245987696180cc7c4587
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-12 14:52:25 +0100

    add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import './App.css'
 // import "./index.css"
 import Navbar from './components/Navbar/Navbar'
 import Carousel from './components/Carousel/Carousel'
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
     <div className='w-full h-full   '>
       <Navbar />
-      <main className="max-w-[1640px] m-auto flex flex-col justify-start items-center gap-4 ">
-        <Wrap />
+      <main className=" max-width  pt-12 lg:p-12 flex flex-col justify-start items-center gap-8 ">
+        <Wrap /> 
         <Carousel />
       </main >
+      <Footer/>
     </div>
 
   );
