0e9890bbc1733a6c821b14d48521506b6368d865
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-12 14:55:00 +0100

    fix light mode colors

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,14 +19,15 @@ const Navbar = () => {
         };
     }, [prevScrollPos]);
     return (
-        <nav className={`p-3 lg:p-6 w-full shadow-md sticky  z-50 bg-bglight dark:bg-bgdark ${visible ? 'top-0' : '-top-20'} transition duration-300 ease-in-out`}>
+        <nav className={`p-3 py-6 border-b-2 dark:border-white border-black lg:p-6 w-full shadow-md sticky  z-50 bg-bglight dark:bg-bgdark ${visible ? 'top-0' : '-top-20'} transition duration-300 ease-in-out`}>
             <div className="flex justify-between">
                 <div className="flex  items-center gap-2">
-                    <img src="/gdglogo.jpg" className="h-10 px-2 dark:invert bg-blend-overlay rounded-xl" />
-                    <div className="">Google I/O Extended</div>
+
+                    <a href="/" ><img src="/gdglogo.jpg" className="h-10  rounded-xl" /></a>
+                    <div className=""><a href="/"> Google I/O Extended </a></div>
                 </div>
                 <div>
-                    <button className="px-4 p-2 rounded-xl hover:border-white border-4 border-transparent  hover:bg-gdggreen-600 dark:hover:bg-gdggreen-500 dark:text-white dark:bg-gdggreen-500 bg-gdggreen-500">
+                    <button className="px-4 p-2 rounded-xl dark:hover:border-white border-2 border-transparent hover:border-black hover:text-black dark:hover:text-white  hover:bg-gdggreen-600 dark:hover:bg-gdggreen-600 text-white dark:bg-gdggreen-500 bg-gdggreen-500">
                         <a href="https://linktoform.com">Join Us here!</a>
                     </button>
                 </div>
