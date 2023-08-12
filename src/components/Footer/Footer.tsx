ef0cb39445b43d6fc3bf754e785e917cf39ed457
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-12 14:54:15 +0100

    add actual content to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,23 @@
+import social_items from "./social_items"
 const Footer = () => {
     return (
-        <div >
-
-        </div>
+        <footer id="footer" className="bottom-0 border-t border-gray-400/30 w-full p-12 lg:py-10 px-7 lg:px-16 bg-grey-900 flex flex-col lg:flex-rowl justify-center lg:justify-between lg:items-center w-max-full">
+            <div className="flex flex-col lg:flex-row justify-between w-full items-first lg:items-center gap-12 lg:gap-2">
+                <div className="flex items-center gap-4">
+                    <img src="/gdglogo.jpg" className="w-8 h-8 dark:mix-blend-normal rounded-full" />
+                    GDG Medea
+                </div>
+                <div className="flex gap-4 ">
+                    {social_items.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <a href={item.url} className="hover:text-gray-500 dark:hover:text-gray-500"><item.logo size={32} /></a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </footer>
     )
 }
 export default Footer
