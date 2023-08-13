1861fbdaddd5cade8833c19cde58d8cb7a8451d8
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-13 12:38:25 +0100

    use image modules src instead of text

--- src/components/Carousel/items.tsx
+++ src/components/Carousel/items.tsx
@@ -1,22 +1,24 @@
-
+import Speaker1 from "../../assets/speakers/speaker1.png"
+import Speaker2 from "../../assets/speakers/speaker2.png"
+import Speaker3 from "../../assets/speakers/speaker3.png"
 const items = [
     {
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        img: "speakers/speaker1.png",
+        img: Speaker1,
         color: "bg-gdgyellow-500"
 
     },
     {
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        img: "speakers/speaker2.png",
+        img: Speaker2,
         color: "bg-gdgred-500"
     },
     {
         name: "Lorem ipsum",
         bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        img: "speakers/speaker3.png",
+        img: Speaker3,
         color: "bg-gdgblue-500"
     }
 ]
