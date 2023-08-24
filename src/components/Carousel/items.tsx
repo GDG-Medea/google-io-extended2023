6abe91ac77d3f0e5b93dc93844380af88dd35997
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-24 23:22:13 +0100

    replace dummy data with actual speakers data

--- src/components/Carousel/items.tsx
+++ src/components/Carousel/items.tsx
@@ -1,23 +1,23 @@
-import Speaker1 from "../../assets/speakers/speaker1.png"
-import Speaker2 from "../../assets/speakers/speaker2.png"
-import Speaker3 from "../../assets/speakers/speaker3.png"
+import Speaker1 from "../../assets/speakers/nabil_hadji_ahmed.jpeg"
+import Speaker2 from "../../assets/speakers/islem_amrouche.webp"
+import Speaker3 from "../../assets/speakers/metidji.jpg"
 const items = [
     {
-        name: "Lorem ipsum",
-        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Dr.Nabil Hadj-Ahmed",
+        bio: "Our speaker Dr.Nabil is a Google Cloud guru with 12 certifications and 20+ years of IT experience. He trains and guides teams on Google Cloud, and leads and speaks at Google Developers events. ",
         img: Speaker1,
-        color: "bg-gdgyellow-500"
+        color: "bg-yellow-500"
 
     },
     {
-        name: "Lorem ipsum",
-        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Islem Amrouche",
+        bio: "Our second speaker Islem is a Full Stack Developer with experience in various technologies (Java EE, Django and React). He works as a Developer Analyst and has a solid background in networking.",
         img: Speaker2,
         color: "bg-gdgred-500"
     },
     {
-        name: "Lorem ipsum",
-        bio: "Lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Mohamed Amine Metidji",
+        bio: "Amine is a Computer science student and a freelance frontend web developer who has skills in HTML, CSS, JavaScript, and React.",
         img: Speaker3,
         color: "bg-gdgblue-500"
     }
