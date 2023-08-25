0e6732f1b4670dda1e4671421d07cfe3faf847ed
momo <githubgdgmedea@gmail.com>
2023-08-25 15:05:39 +0100

    adding topics

--- src/components/Topics/topicsObjct.tsx
+++ src/components/Topics/topicsObjct.tsx
@@ -1,5 +1,9 @@
 import cloudImg from "/src/assets/stacks_cloud.svg";
 import cloudHoverImg from "/src/assets/stacks_cloud-hover.svg";
+import mobileImg from "/src/assets/stacks_mobile.svg";
+import mobileHoverImg from "/src/assets/stacks_mobile-hover.svg";
+import aiImg from "/src/assets/stacks_ai.svg";
+import aiHoverImg from "/src/assets/stacks_ai-hover.svg";
 export const topics = [
     {
       img: cloudImg,
@@ -8,14 +12,14 @@ export const topics = [
       disc: "Simplify and scale end-to-end development.",
     },
     {
-      img: cloudImg,
-      imgHvr: cloudHoverImg,
-      title: "Web",
-      disc: "Create fast, secure sites and apps for the open web.",
+      img: mobileImg,
+      imgHvr: mobileHoverImg,
+      title: "Mobile",
+      disc: "Develop for a range of audiences and form factors.",
     },
     {
-      img: cloudImg,
-      imgHvr: cloudHoverImg,
+      img: aiImg,
+      imgHvr: aiHoverImg,
       title: "AI",
       disc: "Bring the power of machine learning to apps and workflows.",
     },
